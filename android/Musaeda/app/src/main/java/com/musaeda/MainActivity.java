0a51dddf9aad9d2e677eaf82ea4d3e17cf7760ad
sudoalgorithm <kunalmalhotra97@gmail.com>
2017-04-14 17:54:03 +0400

    android update

--- android/Musaeda/app/src/main/java/com/musaeda/MainActivity.java
+++ android/Musaeda/app/src/main/java/com/musaeda/MainActivity.java
@@ -1,12 +1,23 @@
 package com.musaeda;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+  private Context mContext;
+
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    mContext = this;
+    new Handler().postDelayed(new Runnable() {
+      @Override public void run() {
+        startActivity(new Intent(mContext, PickActivity.class));
+      }
+    }, 1000);
   }
 }
--- android/Musaeda/app/src/main/java/com/musaeda/PickActivity.java
+++ android/Musaeda/app/src/main/java/com/musaeda/PickActivity.java
@@ -2,11 +2,16 @@ package com.musaeda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class PickActivity extends AppCompatActivity {
+
+  private ListView mListView;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_pick);
+
+    mListView = (ListView) findViewById(R.id.list_view_pick);
   }
 }
