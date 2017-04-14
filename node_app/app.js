dfde79dcee93f05e2300e9140d2b9465cbdee0e6
npentrel <npentrel@gmail.com>
2017-04-14 18:00:17 +0400

    Sending Twilio message

--- node_app/app.js
+++ node_app/app.js
@@ -5,10 +5,6 @@ const app = require('express')();
 const env = require('node-env-file');
 const assert = require('assert');
 
-// DB Connection stuff
-var MongoClient = require('mongodb').MongoClient;
-var ObjectId = require('mongodb').ObjectID;
-
 env(__dirname + '/.env');
 
 const TWILIO_NUMBER = process.env.TWILIO_NUMBER;
@@ -16,6 +12,30 @@ const user = process.env.MONGO_USER;
 const pass = process.env.MONGO_PW;
 const host = process.env.MONGO_HOST;
 const port = process.env.MONGO_PORT;
+
+
+// DB Connection stuff
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+var twilio = require('twilio');
+
+var accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
+var authToken = process.env.TWILIO_AUTH_TOKEN;   // Your Auth Token from www.twilio.com/console
+
+
+var twilio = require('twilio');
+var client = new twilio.RestClient(accountSid, authToken);
+
+client.messages.create({
+    body: 'Hello from Node',
+    to: process.env.RASHIQ_NUMBER,  // Text this number
+    from: process.env.TWILIO_PHONE_NUMBER // From a valid Twilio number
+}, function(err, message) {
+   if(err) {
+      console.error(err.message);
+   }
+});
+
 
 // On initialization, store associations
 function storeEdges(sender, subscribers) {
@@ -40,11 +60,11 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.post('/store', function(sReq, sRes){    
+app.post('/store', function(sReq, sRes){
   console.log('receiving request ' + sReq);
 })
 
-app.get('/receive', function(sReq, sRes){    
+app.get('/receive', function(sReq, sRes){
   console.log('receiving request ' + sReq);
 })
 
