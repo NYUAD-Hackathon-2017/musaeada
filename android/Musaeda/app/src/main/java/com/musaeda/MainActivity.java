fd282134d032fe3695da0a270b35293906426764
Rashiq <rashiq.z@gmail.com>
2017-04-15 09:22:36 +0400

    Remove handler

--- android/Musaeda/app/src/main/java/com/musaeda/MainActivity.java
+++ android/Musaeda/app/src/main/java/com/musaeda/MainActivity.java
@@ -1,23 +1,14 @@
 package com.musaeda;
 
-import android.content.Context;
 import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-  private Context mContext;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    mContext = this;
-    new Handler().postDelayed(new Runnable() {
-      @Override public void run() {
-        startActivity(new Intent(mContext, PickActivity.class));
-      }
-    }, 1000);
+    startActivity(new Intent(this, PickActivity.class));
   }
 }
