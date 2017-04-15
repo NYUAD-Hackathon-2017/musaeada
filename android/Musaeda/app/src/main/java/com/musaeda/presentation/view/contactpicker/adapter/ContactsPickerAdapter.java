0601c1493c4dfb20a48d20d3241eb15652833a05
Rashiq <rashiq.z@gmail.com>
2017-04-16 00:12:35 +0400

    Beautify the contacts list

--- android/Musaeda/app/src/main/java/com/musaeda/presentation/view/contactpicker/adapter/ContactsPickerAdapter.java
+++ android/Musaeda/app/src/main/java/com/musaeda/presentation/view/contactpicker/adapter/ContactsPickerAdapter.java
@@ -1,18 +1,32 @@
 package com.musaeda.presentation.view.contactpicker.adapter;
 
 import android.content.Context;
-import android.support.v4.content.ContextCompat;
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.*;
 import android.widget.ImageView;
 import android.widget.TextView;
+import com.amulyakhare.textdrawable.TextDrawable;
 import com.musaeda.R;
 import com.musaeda.domain.entity.ContactEntity;
+import com.musaeda.presentation.view.utils.CircleTransform;
 import com.squareup.picasso.Picasso;
 import java.util.List;
+import java.util.Random;
 
 public class ContactsPickerAdapter extends RecyclerView.Adapter<ContactsPickerAdapter.ViewHolder> {
   private List<ContactEntity> data;
+
+  private static final int[] COLORS = {
+      Color.parseColor("#F44336"),
+      Color.parseColor("#E91E63"),
+      Color.parseColor("#9C27B0"),
+      Color.parseColor("#673AB7"),
+      Color.parseColor("#3F51B5"),
+      Color.parseColor("#2196F3"),
+  };
+
+  private static final Random RANDOM = new Random();
 
   public ContactsPickerAdapter(List<ContactEntity> data) {
     this.data = data;
@@ -29,14 +43,27 @@ public class ContactsPickerAdapter extends RecyclerView.Adapter<ContactsPickerAd
 
   @Override
   public void onBindViewHolder(ViewHolder holder, int position) {
+    holder.itemView.setClickable(true);
     ContactEntity entity = data.get(position);
     holder.name.setText(entity.getName());
     Context context = holder.itemView.getContext();
+
+    TextDrawable defaultPhoto = getDefaultDrawable(entity);
+
     if (entity.getPhotoUri() != null) {
-      Picasso.with(context).load(entity.getPhotoUri()).into(holder.photo);
+      Picasso.with(context)
+          .load(entity.getPhotoUri())
+          .transform(new CircleTransform())
+          .into(holder.photo);
     } else {
-      holder.photo.setImageDrawable(ContextCompat.getDrawable(context, R.mipmap.ic_launcher));
+      holder.photo.setImageDrawable(defaultPhoto);
     }
+  }
+
+  private TextDrawable getDefaultDrawable(ContactEntity entity) {
+    String iconText = String.valueOf(entity.getName().charAt(0));
+    int color = COLORS[RANDOM.nextInt(COLORS.length)];
+    return TextDrawable.builder().buildRound(iconText, color);
   }
 
   @Override
