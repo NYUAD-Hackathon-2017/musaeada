b9db7df20469f1426ebe681dc9055dfe69ea5f21
sudoalgorithm <kunalmalhotra97@gmail.com>
2017-04-16 11:06:41 +0400

    SMSSending Activity Completed

--- android/Musaeda/app/src/main/java/com/musaeda/SMSSendingActivity.java
+++ android/Musaeda/app/src/main/java/com/musaeda/SMSSendingActivity.java
@@ -1,8 +1,12 @@
 package com.musaeda;
 
+import android.Manifest;
 import android.os.AsyncTask;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import java.io.BufferedInputStream;
@@ -13,6 +17,8 @@ import java.net.URL;
 public class SMSSendingActivity extends AppCompatActivity {
 
   private Button mbtn1, mbtn2, mbtn3;
+  private String number = "+15876002133";
+  private final static String TAG = "SMSSendingActivity";
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -22,54 +28,59 @@ public class SMSSendingActivity extends AppCompatActivity {
     mbtn2 = (Button) findViewById(R.id.btn_2);
     mbtn3 = (Button) findViewById(R.id.btn_3);
 
+    ActivityCompat.requestPermissions(this,new String[]{ Manifest.permission.SEND_SMS},1);
+
     mbtn1.setOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View v) {
+        SmsManager.getDefault().sendTextMessage(number,null,"SEND I need Help. \n My GPS location (Longitude: 24.522491900000002, \nLatitude: 54.4355024)",null, null);
+        Log.d(TAG,"I need Help");
 
       }
     });
 
     mbtn2.setOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View v) {
-
-
+        SmsManager.getDefault().sendTextMessage(number,null,"SEND I'm Safe",null, null);
+        Log.d(TAG,"I am safe");
       }
     });
 
     mbtn3.setOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View v) {
-
+        SmsManager.getDefault().sendTextMessage(number,null,"SEND Call me",null, null);
+        Log.d(TAG,"Call me");
       }
     });
   }
 
-  public class onBtnClickHttpPost extends AsyncTask<String, String, String> {
-
-    @Override protected void onPreExecute() {
-      super.onPreExecute();
-    }
-
-    @Override protected String doInBackground(String... params) {
-      String urlString = "";
-
-      InputStream in = null;
-      try {
-
-        URL url = new URL(urlString);
-
-        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-
-        in = new BufferedInputStream(urlConnection.getInputStream());
-
-      } catch (Exception e) {
-
-        e.printStackTrace();
-
-        return e.getMessage();
-      }
-
-      return null;
-    }
-
-
-  }
+  //public class onBtnClickHttpPost extends AsyncTask<String, String, String> {
+  //
+  //  @Override protected void onPreExecute() {
+  //    super.onPreExecute();
+  //  }
+  //
+  //  @Override protected String doInBackground(String... params) {
+  //    String urlString = "";
+  //
+  //    InputStream in = null;
+  //    try {
+  //
+  //      URL url = new URL(urlString);
+  //
+  //      HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+  //
+  //      in = new BufferedInputStream(urlConnection.getInputStream());
+  //
+  //    } catch (Exception e) {
+  //
+  //      e.printStackTrace();
+  //
+  //      return e.getMessage();
+  //    }
+  //
+  //    return null;
+  //  }
+  //
+  //
+  //}
 }
--- android/Musaeda/app/src/main/java/com/musaeda/presentation/view/home/MainActivity.java
+++ android/Musaeda/app/src/main/java/com/musaeda/presentation/view/home/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import com.musaeda.R;
+import com.musaeda.SMSSendingActivity;
 import com.musaeda.presentation.view.contactpicker.ContactsPickerActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    startActivity(new Intent(this, ContactsPickerActivity.class));
+    startActivity(new Intent(this, SMSSendingActivity.class));
     finish();
   }
 }
