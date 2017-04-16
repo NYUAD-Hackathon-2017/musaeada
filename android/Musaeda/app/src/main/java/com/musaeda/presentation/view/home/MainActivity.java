6bfb169d9ca03a064e21acb7322c4bcbac1deba9
Rashiq <rashiq.z@gmail.com>
2017-04-16 09:14:31 +0400

    Add home activity

--- android/Musaeda/app/src/main/java/com/musaeda/presentation/view/home/MainActivity.java
+++ android/Musaeda/app/src/main/java/com/musaeda/presentation/view/home/MainActivity.java
@@ -1,19 +1,68 @@
 package com.musaeda.presentation.view.home;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.*;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import com.musaeda.R;
-import com.musaeda.presentation.view.contactpicker.ContactsPickerActivity;
+import com.musaeda.domain.entity.ContactEntity;
+import com.musaeda.presentation.di.components.ApplicationComponent;
+import com.musaeda.presentation.view.BaseActivity;
+import com.musaeda.presentation.view.home.adapter.MyContactsAdapter;
+import com.soundcloud.lightcycle.*;
+import java.util.List;
+import javax.inject.Inject;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity<MainActivity>
+    implements LightCycleDispatcher<ActivityLightCycle<MainActivity>>,
+    HomeCallback {
+
+  @Inject @LightCycle HomePresenter presenter;
+  @BindView(R.id.toolbar) Toolbar toolbar;
+  @BindView(R.id.my_contacts_list) RecyclerView recyclerView;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+  }
+
+  @Override protected void setContentView() {
     setContentView(R.layout.activity_main);
-    startActivity(new Intent(this, ContactsPickerActivity.class));
-    finish();
+    ButterKnife.bind(this);
+    setSupportActionBar(toolbar);
+
+    ActionBar actionBar = getSupportActionBar();
+    if (actionBar != null) {
+      actionBar.setDisplayShowTitleEnabled(false);
+    }
+  }
+
+  @Override protected void setupDagger(ApplicationComponent appComponent) {
+    appComponent.inject(this);
+  }
+
+  @Override public void showLoading() {
+
+  }
+
+  @Override public void hideLoading() {
+
+  }
+
+  @Override public void showError(String message) {
+
+  }
+
+  @Override public void setUpRecyclerView() {
+    recyclerView.setHasFixedSize(true);
+    recyclerView.setLayoutManager(new LinearLayoutManager(this));
+  }
+
+  @Override public void setAdapter(List<ContactEntity> entities) {
+    MyContactsAdapter adapter = new MyContactsAdapter(entities);
+    adapter.setListener(position -> {
+
+    });
+    recyclerView.setAdapter(adapter);
   }
 }
